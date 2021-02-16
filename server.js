31283c0308e15308b00aef9eb91f5fa01d1b2b47
gurinanatalia <gurinanatalia@list.ru>
2021-02-16 17:12:49 +0300

    added shop

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url);
     const body = req.url === '/css/style.css' ?
-        fs.readFileSync('./public/css/style.css', 'utf8') :
+        fs.readFileSync('./public/sass/style.css', 'utf8') :
         fs.readFileSync('./public/index.html', 'utf8');
-    // const body = fs.readFileSync('./public/index.html', 'utf8');
     res.end(body);
 });
 
